7a217d47357d0106d6dcafae5a1bb66e9bcbb068
ArishMada <arishmadataly@gmail.com>
2023-05-26 10:42:12 +0700

    connecting signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,19 +1,71 @@
-import "./Signup.css"
+import "./Signup.css";
 import { FaDoorClosed } from "react-icons/fa";
+import React, { useState } from "react";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    // Make the HTTP request to the signup API
+    try {
+      const response = await fetch("http://localhost:8000/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: email,
+          password: password,
+        }),
+      });
+
+      if (response.ok) {
+        // Signup successful
+        console.log("Signup successful");
+        setError("Successfull");
+        // Perform any desired actions after successful signup
+      } else {
+        // Handle error response from the API
+        const errorData = await response.json();
+        setError(errorData.detail);
+      }
+    } catch (error) {
+      console.log("Error:", error);
+
+      // Handle network error
+      setError("Network error occurred. Please try again.");
+    }
+  };
+
   return (
     <div className="signup">
-      <div className="icon"><FaDoorClosed/> </div>  
+      <div className="icon">
+        <FaDoorClosed />
+      </div>
       <h1>Signup</h1>
-      <form>
-        <input autoFocus type={"email"} placeholder={"Email"}/>
-        <input type={"password"} placeholder={"Password"}/>
-        {/* <input type={"password"} placeholder={"Confirm password"}/> */}
-        <button type={"submit"}>Sign up</button>
+      <form onSubmit={handleSignup}>
+        <input
+          autoFocus
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Sign up</button>
+        {error && <p className="error">{error}</p>}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
