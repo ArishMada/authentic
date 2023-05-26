ca241ab4ebca076b8c56aed1e9be56372a23f066
Andrean2305 <91464375+Andrean2305@users.noreply.github.com>
2023-05-26 10:28:18 +0700

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,90 @@
-import "./Login.css"
+import React, { useState } from "react";
 import { FaDoorOpen } from "react-icons/fa";
+import "./Login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [greeting, setGreeting] = useState("");
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    // Make the HTTP request to the login API
+    try {
+      const response = await fetch("http://localhost:8000/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          username: email,
+          password: password,
+          grant_type: "password",
+          scope: "",
+        }),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        // Access token is available in data.access_token
+        console.log("Access Token:", data.access_token);
+        // Store the email address
+        const userEmail = email;
+        // Set the logged-in state to true
+        setLoggedIn(true);
+        // Set the greeting message
+        setGreeting(`Hello ${userEmail}`);
+        // Clear any previous error
+        setError("");
+      } else {
+        // Handle error response from the API
+        const errorData = await response.json();
+        setError(errorData.detail);
+      }
+    } catch (error) {
+      console.log("Error:", error);
+      // Handle network error
+      setError("Network error occurred. Please try again.");
+    }
+  };
+  
+  
+
   return (
     <div className="login">
-      <div className="icon"><FaDoorOpen/> </div>  
+      <div className="icon">
+        <FaDoorOpen />
+      </div>
       <h1>Login</h1>
-      <form>
-        <input autoFocus type={"email"} placeholder={"Email"}/>
-        <input type={"password"} placeholder={"Password"}/>
-        <button type={"submit"}>Login</button>
-      </form>
+      {!loggedIn ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            autoFocus
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+          {error && <p className="error">{error}</p>}
+        </form>
+      ) : (
+        <>
+        {loggedIn && <h2>{greeting}</h2>}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
